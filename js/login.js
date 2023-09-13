a5e0f057f02b3ceaaa32b3c692dccaab6a0e5c29
satyam46020 <satyam46020@gmail.com>
2023-09-13 23:32:49 +0530

    signin signup

--- js/login.js
+++ js/login.js
@@ -7,18 +7,41 @@ function formfn(e){
     var target=e.target
     var email=target.querySelector("#email").value
     var password=target.querySelector("#password").value
-
-    arr.forEach(e => {
-        if(e.email==email&&e.password==password){
-            // window.location="index.html"
-            var login=document.querySelector(".login")
-            login.innerHTML="logged in"
+    var flag=0
+    if(email==""||password=="")
+    {
+        var h5=document.querySelector("h5")
+        h5.innerHTML="Email or Password can't be Empty!"
+    }
+    else
+    {
+        var flag=0
+        arr.forEach(e => {
+            if(e.email==email&&e.password==password){
+                var login=document.querySelector(".login")
+                var h5=document.querySelector("h5")
+                h5.innerHTML="Logged in Please start shopping!"
+                // window.location="index.html"
+                flag=1;
+                return
+            }
+            if(e.email==email&&e.password!=password)
+            {
+                var h5=document.querySelector("h5")
+                h5.innerHTML="Wrong Password!"
+                flag=1
+                return
+            }
+        })
+        if(flag==0)
+        {
+            var h5=document.querySelector("h5")
+            h5.innerHTML="Not Registered please Sign Up!"
         }
-    })
-    
+    }
+}
     var signup=document.querySelector(".signup").addEventListener("click",signupfn)
 
     function signupfn(){
         window.location="signup.html"
     }
-}
--- js/signup.js
+++ js/signup.js
@@ -1,25 +1,45 @@
-var arr=JSON.parse(localStorage.getItem("userData")) || []
 
-document.querySelector("form").addEventListener("submit",formfn)
+document.querySelector("form").addEventListener("submit", formfn)
 
-function formfn(e){
+var arr = JSON.parse(localStorage.getItem("userData")) || []
+
+function formfn(e) {
     e.preventDefault();
-    var target=e.target
-    var email=target.querySelector("#email").value
-    var password=target.querySelector("#password").value
-    
-    var obj={
+    var target = e.target
+    var email = target.querySelector("#email").value
+    var password = target.querySelector("#password").value
+
+    var obj = {
         email,
         password
     }
-    arr.push(obj)
+    var temp=0
+    arr.forEach(ele => {
+        if(obj.email == ele.email)
+        {
+            temp=1;
+        }
+        
+    });
+    var h5=document.querySelector("h5")
+    if(obj.email==""||obj.password=="")
+    {
+        h5.innerHTML="Email or Password can't be Empty!"
+    }
+    else if(temp==0)
+    {
+        arr.push(obj)
     
-    localStorage.setItem("userData",JSON.stringify(arr))
-
-    var login=document.querySelector(".login").addEventListener("click",loginfn)
-    window.location="login.html"
-
-    function loginfn(){
-        window.location="login.html"
+        localStorage.setItem("userData", JSON.stringify(arr))
+        h5.innerHTML="Signed up please Log in!"
+    }
+    else
+    {
+        h5.innerHTML="Already Registered Please Log in!"
     }
 }
+    var login = document.querySelector(".login").addEventListener("click", loginfn)
+
+    function loginfn() {
+        window.location = "login.html"
+    }
